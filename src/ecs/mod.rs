b948fd7f3a29d4e3eb3abb78c26e89348af316e9
Jingkai Mao <oammix@gmail.com>
2016-11-23 22:49:20 +0800

    Drop internal components immediately after destroying entity

--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -13,14 +13,17 @@ pub type Entity = Handle;
 #[cfg(test)]
 mod test {
     use super::*;
+    use std::sync::{Arc, RwLock};
 
-    #[derive(Debug, Copy, Clone, PartialEq, Eq)]
+
+    #[derive(Debug, Clone, PartialEq, Eq)]
     struct Position {
         x: i32,
         y: i32,
     }
 
     declare_component!(Position, HashMapStorage);
+    declare_component!(Reference, HashMapStorage);
 
     #[test]
     fn basic() {
@@ -50,10 +53,22 @@ mod test {
         assert_eq!(world.fetch::<Position>(e1), None);
     }
 
+    #[derive(Debug, Clone)]
+    struct Reference {
+        value: Arc<RwLock<usize>>,
+    }
+
+    impl Drop for Reference {
+        fn drop(&mut self) {
+            *self.value.write().unwrap() += 1;
+        }
+    }
+
     #[test]
     fn free() {
         let mut world = World::new();
         world.register::<Position>();
+        world.register::<Reference>();
 
         let e1 = world.create();
         assert!(world.is_alive(e1));
@@ -68,6 +83,26 @@ mod test {
         assert!(!world.is_alive(e1));
         assert!(!world.has::<Position>(e1));
         assert_eq!(world.fetch::<Position>(e1), None);
+
+        let mut entities = Vec::new();
+        let rc = Arc::new(RwLock::new(0));
+        for i in 0..10 {
+            let e = world.create();
+            let shadow = rc.clone();
+            entities.push(e);
+
+            world.assign::<Reference>(e, Reference { value: shadow });
+            if i % 2 == 0 {
+                world.assign::<Position>(e, Position { x: 1, y: 2 });
+            }
+        }
+
+        assert_eq!(*rc.read().unwrap(), 0);
+        for i in 0..10 {
+            world.free(entities[i]);
+            assert_eq!(*rc.read().unwrap(), i + 1);
+        }
+        assert_eq!(*rc.read().unwrap(), 10);
     }
 
     #[test]
