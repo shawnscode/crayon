71fdf3603d30bbd4f63ea0bd21988206a576ab88
maojingkai <oammix@gmail.com>
2018-09-06 09:48:57 +0800

    Adds `VariantStr/VariantVec` for small heap object optimization.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -7,10 +7,13 @@ pub mod handle_pool;
 pub mod hash;
 pub mod hash_value;
 pub mod object_pool;
-pub mod variant;
+pub mod variant_str;
+pub mod variant_vec;
 
 pub use self::data_buf::{DataBuffer, DataBufferPtr};
 pub use self::handle::{Handle, HandleIndex};
 pub use self::handle_pool::HandlePool;
+pub use self::hash::{FastHashMap, FastHashSet};
 pub use self::hash_value::HashValue;
-pub use self::variant::{VariantChar, VariantStr};
+pub use self::variant_str::VariantStr;
+pub use self::variant_vec::VariantVec;
