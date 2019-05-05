c93cddf2f99036a34ad5cd521820c994b0303dea
maojingkai <oammix@gmail.com>
2019-05-05 21:03:51 +0800

    Fix bounds check when updating texture.

--- src/video/backends/gl/visitor.rs
+++ src/video/backends/gl/visitor.rs
@@ -471,7 +471,7 @@ impl Visitor for GLVisitor {
             bail!("Trying to update compressed texture.");
         }
 
-        if data.len() > area.volume() as usize
+        if data.len() > texture.params.format.size(area.dim()) as usize
             || area.min.x >= texture.params.dimensions.x
             || area.min.y >= texture.params.dimensions.y
         {
